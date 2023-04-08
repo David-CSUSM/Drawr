8e8eabb4ff46df335f34986544f775b34dc9ae05
David-CSUSM <herna1334@csusm.edu>
2023-04-08 14:59:41 -0700

    Initial Project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+## The Binary Bandits
+Simon Hausmaninger, Adam Beck, David Hernandez
+<br/>
+
 # Drawr
+### Drawr is an editor for animated sprites and pixel art
+<br/>
---
+++ app/src/main/java/com/thebinarybandits/drawr/AppController.java
@@ -0,0 +1,14 @@
+package com.thebinarybandits.drawr;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AppController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
