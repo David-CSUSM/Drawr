7a611f19240cad3746dad2bcb51f0516835f7de0
Adam Beck <beck065@csusm.edu>
2023-04-23 12:18:14 -0700

    Working PaintBucket

--- app/src/main/java/com/thebinarybandits/drawr/tools/PaintBucket.java
+++ app/src/main/java/com/thebinarybandits/drawr/tools/PaintBucket.java
@@ -6,16 +6,17 @@ import javafx.scene.paint.Color;
 public class PaintBucket implements Tool{
     public void useTool(Layer panel, int x, int y, Color color, int scale, int CANVAS_SIZE) {
         // color this square if it is different from color
-        if (!color.equals((panel.getPixelData(x, y))));
+        if (!color.equals((panel.getPixelData(x, y)))) {
             panel.draw(x, y, color);
             // recursivley color all adjacent squares that are not color
-            if (x + scale < CANVAS_SIZE)
-                this.useTool(panel, x + scale, y, color, scale, CANVAS_SIZE);
-            if (x - scale >= 0)
-                this.useTool(panel, x - scale, y, color, scale, CANVAS_SIZE);
-            if (y + scale < CANVAS_SIZE)
-                this.useTool(panel, x, y + scale, color, scale, CANVAS_SIZE);
-            if (y - scale >= 0)
-                this.useTool(panel, x, y - scale, color, scale, CANVAS_SIZE);
+            if (x + 1 < CANVAS_SIZE)
+                this.useTool(panel, x + 1, y, color, scale, CANVAS_SIZE);
+            if (x - 1 >= 0)
+                this.useTool(panel, x - 1, y, color, scale, CANVAS_SIZE);
+            if (y + 1 < CANVAS_SIZE)
+                this.useTool(panel, x, y + 1, color, scale, CANVAS_SIZE);
+            if (y - 1 >= 0)
+                this.useTool(panel, x, y - 1, color, scale, CANVAS_SIZE);
+        }
     }
 }
