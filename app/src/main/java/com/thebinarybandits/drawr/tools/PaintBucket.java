9fbdd344612298cdb2bedadafa84ce6e1457aa60
Adam Beck <beck065@csusm.edu>
2023-04-23 17:21:24 -0700

    Fixed PaintBucket

--- app/src/main/java/com/thebinarybandits/drawr/tools/PaintBucket.java
+++ app/src/main/java/com/thebinarybandits/drawr/tools/PaintBucket.java
@@ -5,18 +5,32 @@ import javafx.scene.paint.Color;
 
 public class PaintBucket implements Tool{
     public void useTool(Layer panel, int x, int y, Color color, int CANVAS_SIZE) {
-        // color this square if it is different from color
-        if (!color.equals((panel.getPixelData(x, y)))) {
-            panel.draw(x, y, color);
-            // recursivley color all adjacent squares that are not color
+        Color paneColor = panel.getPixelData(x, y);
+        panel.draw(x, y, color);
+        
+        // recursively color squares that are the same color as panecolor
+        if (x + 1 < CANVAS_SIZE)
+            recursivePaint(panel, x + 1, y, color, paneColor, CANVAS_SIZE);
+        if (x - 1 >= 0)
+            recursivePaint(panel, x - 1, y, color, paneColor, CANVAS_SIZE);
+        if (y + 1 < CANVAS_SIZE)
+            recursivePaint(panel, x, y + 1, color, paneColor, CANVAS_SIZE);
+        if (y - 1 >= 0)
+            recursivePaint(panel, x, y - 1, color, paneColor, CANVAS_SIZE);
+    }
+
+    // recursive function for coloring adjacent sqaures that are the same color as the square first clicked on
+    public void recursivePaint(Layer panel, int x, int y, Color paintbucketColor, Color firstPaneColor, int CANVAS_SIZE) {
+        if (firstPaneColor.equals(panel.getPixelData(x, y))) {
+            panel.draw(x, y, paintbucketColor);
             if (x + 1 < CANVAS_SIZE)
-                this.useTool(panel, x + 1, y, color, CANVAS_SIZE);
-            if (x - 1 >= 0)
-                this.useTool(panel, x - 1, y, color, CANVAS_SIZE);
-            if (y + 1 < CANVAS_SIZE)
-                this.useTool(panel, x, y + 1, color, CANVAS_SIZE);
-            if (y - 1 >= 0)
-                this.useTool(panel, x, y - 1, color, CANVAS_SIZE);
+            recursivePaint(panel, x + 1, y, paintbucketColor, firstPaneColor, CANVAS_SIZE);
+        if (x - 1 >= 0)
+            recursivePaint(panel, x - 1, y, paintbucketColor, firstPaneColor, CANVAS_SIZE);
+        if (y + 1 < CANVAS_SIZE)
+            recursivePaint(panel, x, y + 1, paintbucketColor, firstPaneColor, CANVAS_SIZE);
+        if (y - 1 >= 0)
+            recursivePaint(panel, x, y - 1, paintbucketColor, firstPaneColor, CANVAS_SIZE);
         }
     }
 }
