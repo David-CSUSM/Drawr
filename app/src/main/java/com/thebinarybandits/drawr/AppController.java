f8cc42b69143e1884f92bd705451584ec0a03a66
David-CSUSM <herna1334@csusm.edu>
2023-05-12 14:57:18 -0700

    add save as onAction functions

--- app/src/main/java/com/thebinarybandits/drawr/AppController.java
+++ app/src/main/java/com/thebinarybandits/drawr/AppController.java
@@ -1,17 +1,15 @@
 package com.thebinarybandits.drawr;
 
-import java.io.*;
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
 import com.thebinarybandits.drawr.pixelcanvas.PixelCanvas;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.image.WritableImage;
-//import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.control.ButtonType;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.*;
 import java.util.ArrayList;
 
 public class AppController {
@@ -45,17 +43,17 @@ public class AppController {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setDialogTitle("Choose a File");
-    
+
         // Set file extension filter to only show '.drawr' files
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Drawr Files (*.drawr)", "drawr");
         fileChooser.setFileFilter(filter);
-    
+
         // Show the dialog and wait for user response
         int result = fileChooser.showOpenDialog(null);
-    
+
         if (result == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
-    
+
             // Check if selected file is a '.drawr' file
             if (!selectedFile.getName().endsWith(".drawr")) {
                 // Display an error message if selected file is not a '.drawr' file
@@ -63,12 +61,12 @@ public class AppController {
                 alert.showAndWait();
                 return;
             }
-    
+
             FileInputStream fileIn = new FileInputStream(selectedFile.toString());
             ObjectInputStream in = new ObjectInputStream(fileIn);
             ArrayList<String[][]> layersArrayList = (ArrayList<String[][]>) in.readObject();
             in.close();
-    
+
             // if we allow users to change canvas size
             // then we would have to initialize the canvas based on the file's canvas size
             canvas.reset();
@@ -76,36 +74,45 @@ public class AppController {
             canvas.initLayersData(layersArrayList);
         }
     }
-    
 
     @FXML
-    void saveProjectAs(ActionEvent event) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-        
+    void saveAsGIF(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveAsPNG(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveAsSpritesheet(ActionEvent event) {
+
     }
 
     @FXML
     void saveProject(ActionEvent event) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         JFileChooser fileChooser = new JFileChooser();
-    
+
         // Set file extension filter to only show '.drawr' files
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Drawr Files (*.drawr)", "drawr");
         fileChooser.setFileFilter(filter);
-    
+
         // Show the dialog and wait for user response
         int result = fileChooser.showSaveDialog(null);
-    
+
         if (result == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
-            
+
             // Add '.drawr' extension if not already present
             String filePath = selectedFile.getAbsolutePath();
             if (!filePath.endsWith(".drawr")) {
                 selectedFile = new File(filePath + ".drawr");
             }
-    
+
             ArrayList<String[][]> layersArrayList = canvas.getLayersData();
-    
+
             FileOutputStream fileOut = new FileOutputStream(selectedFile.toString());
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             out.writeObject(layersArrayList);
@@ -125,7 +132,7 @@ public class AppController {
     }
 
     @FXML
-    void clear(ActionEvent event){
+    void clear(ActionEvent event) {
         canvas.clear();
     }
 }
