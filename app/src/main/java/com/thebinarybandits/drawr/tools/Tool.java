849141e179c5777464f1cc3d88c1cb9e86bb5cb2
Adam Beck <beck065@csusm.edu>
2023-05-16 18:18:22 -0700

    added javadoc style documentation

--- app/src/main/java/com/thebinarybandits/drawr/tools/Tool.java
+++ app/src/main/java/com/thebinarybandits/drawr/tools/Tool.java
@@ -3,6 +3,10 @@ package com.thebinarybandits.drawr.tools;
 import com.thebinarybandits.drawr.pixelcanvas.PixelImage;
 import javafx.scene.paint.Color;
 
+/**
+ * Tool interface. To be implemented by all tools.
+ * Allows the activeTool data member in PixelCanvas to benefit from polymorphism.
+ */
 public interface Tool {
     public void useTool(PixelImage panel, int x, int y, Color color, int CANVAS_SIZE);
 }
--- app/src/main/java/com/thebinarybandits/drawr/tools/ToolsController.java
+++ app/src/main/java/com/thebinarybandits/drawr/tools/ToolsController.java
@@ -1,19 +1,24 @@
 package com.thebinarybandits.drawr.tools;
 
 import com.thebinarybandits.drawr.pixelcanvas.PixelCanvas;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.RadioButton;
 import javafx.scene.paint.Color;
 
+/**
+ * Interacts with the view_tools xml file.
+ */
 public class ToolsController {
     @FXML
     private ColorPicker colorPicker;
     @FXML
     private RadioButton penButton;
-    private final PixelCanvas canvas = PixelCanvas.getInstance();
+    private final PixelCanvas canvas = PixelCanvas.getInstance(); // the active canvas
 
+    /**
+     * Resets the Tools panel to default values.
+     */
     public void reset(){
         penButton.setSelected(true);
         canvas.setTool(new Pen());
@@ -21,28 +26,48 @@ public class ToolsController {
         canvas.setColor(colorPicker.getValue());
     }
 
+    /**
+     * Runs when the user clicks the eraser button.
+     * Sets the eraser tool as the active tool.
+     */
     @FXML
-    void eraserSelected(ActionEvent event) {
+    void eraserSelected() {
         canvas.setTool(new Eraser());
     }
 
+    /**
+     * Runs when the user clicks the paint bucket button.
+     * Sets the paint bucket tool as the active tool.
+     */
     @FXML
-    void paintBucketSelected(ActionEvent event) {
+    void paintBucketSelected() {
         canvas.setTool(new PaintBucket());
     }
 
+    /**
+     * Runs when the user clicks the pen button.
+     * Sets the pen tool as the active tool.
+     */
     @FXML
-    void penSelected(ActionEvent event) {
+    void penSelected() {
         canvas.setTool(new Pen());
     }
 
+    /**
+     * Runs when the user clicks the eye dropper button.
+     * Sets the eye dropper tool as the active tool.
+     */
     @FXML
-    void eyeDropperSelected(ActionEvent event) {
+    void eyeDropperSelected() {
         canvas.setTool(new EyeDropper(colorPicker, canvas));
     }
 
+    /**
+     * Runs when the user selects a color from the colorp picker.
+     * Sets the color chosen as the active color.
+     */
     @FXML
-    void updateColor(ActionEvent event) {
+    void updateColor() {
         canvas.setColor(colorPicker.getValue());
     }
 }
